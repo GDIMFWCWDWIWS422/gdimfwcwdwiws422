7fb8f5dc68b57fddcbf5c496d7e316c678d8d347
Thomas Ströder <thomas.stroeder@email.de>
2022-10-21 17:18:20 +0100

    methods for singly linked list

--- src/gdi/SinglyLinkedList.java
+++ src/gdi/SinglyLinkedList.java
@@ -34,4 +34,38 @@ class SinglyLinkedList {
         }*/
         }
     }
+    void add(int value) {
+        if(isEmpty()) {
+            root = new SinglyLinkedListElement ();
+            root.value = value;
+        }
+        else {
+            SinglyLinkedListElement current = root;
+            while (current.next != null) {
+                current = current.next;   
+            }
+            
+            current.next = new SinglyLinkedListElement();
+            current.next.value = value;
+       
+        }
+    }
+    void remove(int value) {
+        if(!isEmpty()) {
+            if(root.value==value) {
+                root=root.next;
+            }
+            else {
+            SinglyLinkedListElement current = root;
+            while(current.next!=null && current.next.value!=value) {
+                current=current.next;
+                
+            }
+            if(current.next!=null) {
+                current.next=current.next.next;
+            }
+           }
+        }
+        
+    }
 }
